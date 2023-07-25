a743fb93693e6983f3d7064eb7276419d7cd369f
28904 <28904@KNOBBLEBOY>
2023-07-24 23:29:35 -0600

    Agregar archivos de proyecto.

---
+++ Entry1/Entry1/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Entry1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        ///<Summary>
+        ///<Createddate>24/07/2023</Createddate>
+        ///<company>INDEL</company>
+        ///<lastmodificationdate>24/07/2023</lastmodificationdate>
+        ///<lastmodificationdescription>
+        ///Este programa solo muestra las propiedades basicas de un entry
+        ///
+        ///</lastmodificationdescription>
+        //////<lastmodifierautor>Fernando Jose Majano Aguirre</lastmodifierautor>
+        ///</Summary>
+    }
+}
